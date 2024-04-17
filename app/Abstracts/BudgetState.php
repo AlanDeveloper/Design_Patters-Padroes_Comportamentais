f0ed6d46c014cd900dad3407053578e568ab4571
Alan Santos <alan.santos@yellowgo.com.br>
2024-04-17 15:23:00 -0300

    Implementado padrão de projeto State

--- app/Abstracts/BudgetState.php
+++ app/Abstracts/BudgetState.php
@@ -9,16 +9,37 @@ abstract class BudgetState
 {
     abstract public function calcExtraDiscount(Budget $budget);
 
+    /**
+     * Undocumented function
+     *
+     * @param Budget $budget
+     * @throws DomainException
+     * @return void
+     */
     public function approve(Budget $budget) : void
     {
         throw new DomainException('Este orçamento não pode ser aprovado.');
     }
 
+    /**
+     * Undocumented function
+     *
+     * @param Budget $budget
+     * @throws DomainException
+     * @return void
+     */
     public function reject(Budget $budget) : void
     {
         throw new DomainException('Este orçamento não pode ser reprovado.');
     }
 
+    /**
+     * Undocumented function
+     *
+     * @param Budget $budget
+     * @throws DomainException
+     * @return void
+     */
     public function finish(Budget $budget) : void
     {
         throw new DomainException('Este orçamento não pode ser finalizado.');
